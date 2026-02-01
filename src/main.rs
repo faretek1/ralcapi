1fb810e477511857ab528b1e3ba63e0cf6e84585
rtk <ldashunderscoreo@gmail.com>
2026-02-01 17:20:01 +0000

    feat: add numbers api

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use axum::{
     Json, Router,
+    extract::Path,
     http::StatusCode,
     routing::{get, post},
 }; // copied from https://github.com/tokio-rs/axum#usage-example
@@ -12,7 +13,9 @@ async fn main() {
     // tracing_subscriber::fmt::init();
 
     // build our application with a route
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/add/{a}/{b}", get(add_handler));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -22,4 +25,8 @@ async fn main() {
 async fn root() -> axum::response::Redirect {
     // &'static str
     axum::response::Redirect::to("https://github.com/faretek1/ralcapi")
+}
+
+async fn add_handler(Path((a, b)): Path<(f64, f64)>) -> String {
+    format!("{} + {} = {}", a, b, a + b)
 }
