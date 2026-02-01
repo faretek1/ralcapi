5bec4f5e8459b85594f5ce0aebaf3d1d99c2fcb9
rtk <ldashunderscoreo@gmail.com>
2026-02-01 15:37:33 +0000

    feat: basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json, Router,
+    http::StatusCode,
+    routing::{get, post},
+}; // copied from https://github.com/tokio-rs/axum#usage-example
+
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    // tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new().route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "idk"
 }
