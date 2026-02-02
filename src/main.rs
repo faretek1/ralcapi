37c5b5d63430d0a00e11c8766ef23a0a0651245d
rtk <ldashunderscoreo@gmail.com>
2026-02-02 21:03:46 +0000

    feat: sub/mul/div

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
-use axum::{
-    Json, Router,
-    extract::Path,
-    http::StatusCode,
-    routing::{get, post},
-}; // copied from https://github.com/tokio-rs/axum#usage-example
-
-use serde::{Deserialize, Serialize};
+use axum::{Router, extract::Path, routing::get};
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    // tracing_subscriber::fmt::init();
-
-    // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/add/{a}/{b}", get(add_handler));
+        .route("/add/{a}/{b}", get(|p| calc(p, "+", |a, b| a + b)))
+        .route("/sub/{a}/{b}", get(|p| calc(p, "-", |a, b| a - b)))
+        .route("/mul/{a}/{b}", get(|p| calc(p, "*", |a, b| a * b)))
+        .route("/div/{a}/{b}", get(|p| calc(p, "/", |a, b| a / b)));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -23,10 +15,13 @@ async fn main() {
 }
 
 async fn root() -> axum::response::Redirect {
-    // &'static str
     axum::response::Redirect::to("https://github.com/faretek1/ralcapi")
 }
 
-async fn add_handler(Path((a, b)): Path<(f64, f64)>) -> String {
-    format!("{} + {} = {}", a, b, a + b)
+async fn calc(
+    Path((a, b)): Path<(f64, f64)>,
+    sym: &'static str,
+    op: impl Fn(f64, f64) -> f64,
+) -> String {
+    format!("{} {} {} = {}", a, sym, b, op(a, b))
 }
